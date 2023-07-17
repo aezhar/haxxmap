100faecf5c86de0e6c93acdfa8f4004fbb603c0e
aezhar <104122316+aezhar@users.noreply.github.com>
2023-07-17 15:58:38 +0200

    replace inlined xxhash function with cespare package

--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module github.com/aezhar/haxxmap
 go 1.18
 
 require golang.org/x/exp v0.0.0-20221031165847-c99f073a8326
+
+require github.com/cespare/xxhash v1.1.0 // indirect
--- hash.go
+++ hash.go
@@ -26,10 +26,11 @@ WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
 import (
-	"encoding/binary"
 	"math/bits"
 	"reflect"
 	"unsafe"
+
+	"github.com/cespare/xxhash"
 )
 
 const (
@@ -49,39 +50,11 @@ const (
 	prime5 uint64 = 2870177450012600261
 )
 
-var prime1v = prime1
-
-func u64(b []byte) uint64 { return binary.LittleEndian.Uint64(b) }
-func u32(b []byte) uint32 { return binary.LittleEndian.Uint32(b) }
-
-func round(acc, input uint64) uint64 {
-	acc += input * prime2
-	acc = rol31(acc)
-	acc *= prime1
-	return acc
-}
-
-func mergeRound(acc, val uint64) uint64 {
-	val = round(0, val)
-	acc ^= val
-	acc = acc*prime1 + prime4
-	return acc
-}
-
-func rol1(x uint64) uint64  { return bits.RotateLeft64(x, 1) }
-func rol7(x uint64) uint64  { return bits.RotateLeft64(x, 7) }
-func rol11(x uint64) uint64 { return bits.RotateLeft64(x, 11) }
-func rol12(x uint64) uint64 { return bits.RotateLeft64(x, 12) }
-func rol18(x uint64) uint64 { return bits.RotateLeft64(x, 18) }
-func rol23(x uint64) uint64 { return bits.RotateLeft64(x, 23) }
-func rol27(x uint64) uint64 { return bits.RotateLeft64(x, 27) }
-func rol31(x uint64) uint64 { return bits.RotateLeft64(x, 31) }
-
 // xxHash implementation for known key type sizes, minimal with no branching
 var (
 	// byte hasher, key size -> 1 byte
 	byteHasher = func(key uint8) uintptr {
-		h := prime5 + 1
+		h := prime5 + byteSize
 		h ^= uint64(key) * prime5
 		h = bits.RotateLeft64(h, 11) * prime1
 		h ^= h >> 33
@@ -94,7 +67,7 @@ var (
 
 	// word hasher, key size -> 2 bytes
 	wordHasher = func(key uint16) uintptr {
-		h := prime5 + 2
+		h := prime5 + wordSize
 		h ^= (uint64(key) & 0xff) * prime5
 		h = bits.RotateLeft64(h, 11) * prime1
 		h ^= ((uint64(key) >> 8) & 0xff) * prime5
@@ -109,7 +82,7 @@ var (
 
 	// dword hasher, key size -> 4 bytes
 	dwordHasher = func(key uint32) uintptr {
-		h := prime5 + 4
+		h := prime5 + dwordSize
 		h ^= uint64(key) * prime1
 		h = bits.RotateLeft64(h, 23)*prime2 + prime3
 		h ^= h >> 33
@@ -125,7 +98,7 @@ var (
 	// Example :- casting uint32(1.3) will drop off the 0.3 decimal part but using *(*uint32)(unsafe.Pointer(&key)) will retain all bits (both the integer as well as the decimal part)
 	// this will ensure correctness of the hash
 	float32Hasher = func(key float32) uintptr {
-		h := prime5 + 4
+		h := prime5 + dwordSize
 		h ^= uint64(*(*uint32)(unsafe.Pointer(&key))) * prime1
 		h = bits.RotateLeft64(h, 23)*prime2 + prime3
 		h ^= h >> 33
@@ -141,7 +114,7 @@ var (
 		k1 := key * prime2
 		k1 = bits.RotateLeft64(k1, 31)
 		k1 *= prime1
-		h := (prime5 + 8) ^ k1
+		h := (prime5 + qwordSize) ^ k1
 		h = bits.RotateLeft64(h, 27)*prime1 + prime4
 		h ^= h >> 33
 		h *= prime2
@@ -157,7 +130,7 @@ var (
 		k1 := *(*uint64)(unsafe.Pointer(&key)) * prime2
 		k1 = bits.RotateLeft64(k1, 31)
 		k1 *= prime1
-		h := (prime5 + 8) ^ k1
+		h := (prime5 + qwordSize) ^ k1
 		h = bits.RotateLeft64(h, 27)*prime1 + prime4
 		h ^= h >> 33
 		h *= prime2
@@ -172,7 +145,7 @@ var (
 		k1 := *(*uint64)(unsafe.Pointer(&key)) * prime2
 		k1 = bits.RotateLeft64(k1, 31)
 		k1 *= prime1
-		h := (prime5 + 8) ^ k1
+		h := (prime5 + qwordSize) ^ k1
 		h = bits.RotateLeft64(h, 27)*prime1 + prime4
 		h ^= h >> 33
 		h *= prime2
@@ -191,55 +164,7 @@ func (m *Map[K, V]) setDefaultHasher() {
 		m.hasher = func(key K) uintptr {
 			sh := (*reflect.StringHeader)(unsafe.Pointer(&key))
 			b := unsafe.Slice((*byte)(unsafe.Pointer(sh.Data)), sh.Len)
-			n := sh.Len
-			var h uint64
-
-			if n >= 32 {
-				v1 := prime1v + prime2
-				v2 := prime2
-				v3 := uint64(0)
-				v4 := -prime1v
-				for len(b) >= 32 {
-					v1 = round(v1, u64(b[0:8:len(b)]))
-					v2 = round(v2, u64(b[8:16:len(b)]))
-					v3 = round(v3, u64(b[16:24:len(b)]))
-					v4 = round(v4, u64(b[24:32:len(b)]))
-					b = b[32:len(b):len(b)]
-				}
-				h = rol1(v1) + rol7(v2) + rol12(v3) + rol18(v4)
-				h = mergeRound(h, v1)
-				h = mergeRound(h, v2)
-				h = mergeRound(h, v3)
-				h = mergeRound(h, v4)
-			} else {
-				h = prime5
-			}
-
-			h += uint64(n)
-
-			i, end := 0, len(b)
-			for ; i+8 <= end; i += 8 {
-				k1 := round(0, u64(b[i:i+8:len(b)]))
-				h ^= k1
-				h = rol27(h)*prime1 + prime4
-			}
-			if i+4 <= end {
-				h ^= uint64(u32(b[i:i+4:len(b)])) * prime1
-				h = rol23(h)*prime2 + prime3
-				i += 4
-			}
-			for ; i < end; i++ {
-				h ^= uint64(b[i]) * prime5
-				h = rol11(h) * prime1
-			}
-
-			h ^= h >> 33
-			h *= prime2
-			h ^= h >> 29
-			h *= prime3
-			h ^= h >> 32
-
-			return uintptr(h)
+			return uintptr(xxhash.Sum64(b))
 		}
 	case reflect.Int, reflect.Uint, reflect.Uintptr, reflect.UnsafePointer:
 		switch intSizeBytes {
